0447731f7506a7dff0184d596ea660334d1d6685
Konstantin <konstantin@revimedia.com>
2020-11-08 22:13:55 +0200

    Try to generate jooq over entities

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,6 +1,5 @@
 package guru.springframework;
 
-import guru.springframework.jooq.demo.JooqDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,9 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Spring5RecipeAppApplication.class, args);
-		JooqDemo jooqDemo = context.getBean(JooqDemo.class);
-		jooqDemo.testSelectWithJoin();
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Spring5RecipeAppApplication.class, args);
+    }
 }
